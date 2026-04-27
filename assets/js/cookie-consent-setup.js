1275e4a7dff2689bb21f1dadcccd3821a4ab38ba
Qiaoqian-Fan0214 <yzhao642@connect.hkust-gz.edu.cn>
2026-04-27 15:23:48 +0000

    Deploying to gh-pages from @ Qiaoqian-Fan0214/Yifan.github.io@fcd46743eb90a8ba8902d42da59004c73f858fa2 🚀

--- assets/js/cookie-consent-setup.js
+++ assets/js/cookie-consent-setup.js
@@ -96,7 +96,7 @@ function initializeCookieConsent() {
               },
               {
                 title: 'More information',
-                description: 'For any queries in relation to our policy on cookies and your choices, please <a class="cc-link" href="https://Yifan0214.github.io/#contact">contact us</a>.'
+                description: 'For any queries in relation to our policy on cookies and your choices, please <a class="cc-link" href="https://Qiaoqian-Fan0214.github.io/#contact">contact us</a>.'
               }
             ]
           }
